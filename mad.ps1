0fd6c4b15370f718a96cd7ef7d22e362cedbe96e
dhruv-0512 <dhruvh3012@gmail.com>
2026-05-23 23:47:31 +0530

    fix url

--- mad.ps1
+++ mad.ps1
@@ -1,9 +1,9 @@
-function ask {
-    param([string]$msg)
-    $res = irm $res = irm https://bot-qrxb.onrender.com/chat ` `
+while ($true) {
+    $msg = Read-Host "You"
+    if ($msg -eq "exit") { break }
+    $res = irm https://bot-qrxb.onrender.com/chat `
         -Method POST `
         -ContentType "application/json" `
         -Body "{`"message`": `"$msg`"}"
     Write-Host "`nBot: $($res.reply)`n"
 }
-Write-Host "CS Bot ready. Usage: ask 'your question'"
